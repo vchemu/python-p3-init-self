0cc0eb3ffcb2f496e1832893bf1c2df61824d7a9
vchemu <valerine.chemutai@student.moringaschool.com>
2023-10-10 02:48:40 +0300

    selfinit

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed = "Mutt"):
+        self.name = name
+        self.breed = breed
+
+    def showing_self(self):
+        return self
+    
+fido = Dog("Fido")
+fido.showing_self()
+fido is fido.showing_self()
